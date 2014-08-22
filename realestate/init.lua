8291fa786cad92c2e7d3cd17640d72e4c32c02c3
Tim <t4im@users.noreply.github.com>
2014-08-22 08:47:37 +0200

    implement landrush support for landsale

--- realestate/init.lua
+++ realestate/init.lua
@@ -12,3 +12,8 @@ dofile(economy.realestate.modpath.."/config.lua")
 -- anything related to selling land
 dofile(economy.realestate.modpath.."/landsale.lua")
 dofile(economy.realestate.modpath.."/forsale_signs.lua")
+
+-- landrush support
+if minetest.get_modpath("landrush") then
+dofile(economy.realestate.modpath.."/landrush_landsale.lua")
+end
---
+++ realestate/landrush_landsale.lua
@@ -0,0 +1,11 @@
+local previousTransfer = economy.realestate.transfer
+economy.realestate.transfer = function(pos, node, puncher)
+	local name = puncher:get_player_name()
+	local chunk = landrush.get_chunk(pos)
+	landrush.claims[chunk] = {owner=name, shared={}, claimtype='landclaim'}
+	landrush.save_claims()
+	if previousTransfer then
+		return previousTransfer(pos, node, puncher)
+	end
+	return true
+end
