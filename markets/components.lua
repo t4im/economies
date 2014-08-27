979af1ff0cf0320a0947b4501ba4e715c7718e29
Tim <t4im@users.noreply.github.com>
2014-08-27 20:04:31 +0200

    add several crafting items for later inclusion

---
+++ markets/components.lua
@@ -0,0 +1,19 @@
+minetest.register_craftitem("markets:currency_validator", {
+	description = "Currency validator component",
+	--inventory_image = "",
+})
+
+minetest.register_craftitem("markets:coin_acceptor", {
+	description = "Coin accepting component",
+	--inventory_image = "",
+})
+
+minetest.register_craftitem("markets:note_acceptor", {
+	description = "Banknote accepting component",
+	--inventory_image = "",
+})
+
+minetest.register_craftitem("markets:card_acceptor", {
+	description = "Payment card accepting component",
+	--inventory_image = "",
+})
--- markets/init.lua
+++ markets/init.lua
@@ -9,8 +9,12 @@ economy.markets.modpath = minetest.get_modpath("markets")
 -- load configuraton
 dofile(economy.markets.modpath.."/config.lua")
 
+-- load crafting materials and upgrades
+--dofile(economy.markets.modpath.."/components.lua")
+
 -- load markets
 dofile(economy.markets.modpath.."/market_stalls.lua")
 dofile(economy.markets.modpath.."/bulk_vending_machine.lua")
 dofile(economy.markets.modpath.."/full_line_vending_machine.lua")
 dofile(economy.markets.modpath.."/displays.lua")
+
