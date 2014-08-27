0662ab1adfb8887567ba48170036574725295d50
Tim <t4im@users.noreply.github.com>
2014-08-27 14:59:03 +0200

    reorder and comment config.lua

--- bank/config.lua
+++ bank/config.lua
@@ -1,8 +1,8 @@
--- use economy.conf in your worldpath to change any settings listed as defaults below
-economy.config = economy.config or Settings(minetest.get_worldpath().."/economy.conf")
-
-local conf_table = economy.config:to_table()
-
+--
+-- configuration handling
+--
+-- use economy.conf in your worldpath to change any settings listed as defaults here
+--
 local defaults = {
 	initial_amount = "0",
 	currency_format = "%dcr",
@@ -13,13 +13,12 @@ local defaults = {
 	max_journal_size = "5",
 }
 
+-- configuration slurping
+economy.config = economy.config or Settings(minetest.get_worldpath().."/economy.conf")
+local conf_table = economy.config:to_table()
+
 for k, v in pairs(defaults) do
-	if conf_table[k] == nil then
-		economy.config:set(k, v)
-	end
+	if conf_table[k] == nil then economy.config:set(k, v) end
 end
 
 economy.debug = conf_table["debug"] or minetest.setting_getbool("debug_mods")
-if economy.debug then
-	print("economy mod is in Debugmode")
-end
