9e68ed16b67a1dda780dc6c9ea355c6534c7de3f
Tim <t4im@users.noreply.github.com>
2014-09-01 12:45:19 +0200

    add additional components for machines and pos systems

--- markets/components.lua
+++ markets/components.lua
@@ -1,3 +1,5 @@
+-- currency handling machine parts
+--
 minetest.register_craftitem("markets:currency_validator", {
 	description = "Currency validator component",
 	--inventory_image = "",
@@ -15,5 +17,45 @@ minetest.register_craftitem("markets:note_acceptor", {
 
 minetest.register_craftitem("markets:card_acceptor", {
 	description = "Payment card accepting component",
+	--inventory_image = "",
+})
+
+-- product handling machine parts
+--
+minetest.register_craftitem("markets:vend_counter", {
+	description = "Component counting products that were dispensed/deposited",
+	--inventory_image = "",
+})
+
+-- pos and pos components
+--
+
+minetest.register_craftitem("markets:cash_box", {
+	description = "Mobile strongbox for cash",
+	--inventory_image = "",
+})
+
+minetest.register_craftitem("markets:cash_drawer", {
+	description = "Cash drawer",
+	--inventory_image = "",
+})
+
+minetest.register_craftitem("markets:barcode_reader", {
+	description = "Barcode reader",
+	--inventory_image = "",
+})
+
+minetest.register_craftitem("markets:card_reader", {
+	description = "Payment card reader",
+	--inventory_image = "",
+})
+
+minetest.register_craftitem("markets:receipt_printer", {
+	description = "Receipt printer",
+	--inventory_image = "",
+})
+
+minetest.register_craftitem("markets:cash_register", {
+	description = "Cash register",
 	--inventory_image = "",
 })
