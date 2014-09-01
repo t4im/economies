3851f38e62286ffd919b0ef1a2866b8589793d5c
Tim <t4im@users.noreply.github.com>
2014-09-01 14:54:51 +0200

    replace the forsale-sign lgpl code with the newly written on_place switcher

--- realestate/forsale_signs.lua
+++ realestate/forsale_signs.lua
@@ -1,54 +1,9 @@
--- uses some code, especially the nodeboxmodels and sign type determination, of
--- the rewritten sign_lib by Vanessa Ezekowitz and Diego Martinez under lgpl
-
 -- call the functions that make the landselling happen
 -- not mixing these with the forsale signs allows us to
 -- add other nodes, for example aliased ones from protectionmods
 local forsale_receive_fields = economies.realestate.landsale.receive_fields
 local forsale_punch = economies.realestate.landsale.punch
 local forsale_construct = economies.realestate.landsale.construct
-
-local determine_sign_type = function(itemstack, placer, pointed_thing)
-	local name = minetest.get_node(pointed_thing.above).name
-
-	if not economies.buildableTo(pointed_thing.above, placer, reportViolation) then
-		return itemstack
-	end
-
-	local node = minetest.get_node(pointed_thing.under)
-
-	if minetest.registered_nodes[node.name] and minetest.registered_nodes[node.name].on_rightclick then
-		return minetest.registered_nodes[node.name].on_rightclick(pointed_thing.under, node, placer, itemstack)
-	else
-		local above = pointed_thing.above
-		local under = pointed_thing.under
-		local dir = {x = under.x - above.x,
-			y = under.y - above.y,
-			z = under.z - above.z}
-
-		local walldir = minetest.dir_to_wallmounted(dir)
-
-		local placer_pos = placer:getpos()
-		if placer_pos then
-			dir = {	x = above.x - placer_pos.x,
-				y = above.y - placer_pos.y,
-				z = above.z - placer_pos.z }
-		end
-
-		local facedir = minetest.dir_to_facedir(dir)
-
-		if walldir == 0 then
-			minetest.add_node(above, {name = "realestate:forsale_sign_hanging", param2 = facedir})
-		elseif walldir == 1 then
-			minetest.add_node(above, {name = "realestate:forsale_sign_yard", param2 = facedir})
-		else -- it must be a wall mounted sign.
-			minetest.add_node(above, {name = itemstack:get_name(), param2 = facedir})
-		end
-
-		itemstack:take_item()
-		return itemstack
-	end
-end
 
 minetest.register_node("realestate:forsale_sign", {
 	description = "For Sale sign",
@@ -67,7 +22,10 @@ minetest.register_node("realestate:forsale_sign", {
 		"forsale_sign_borders.png", "forsale_sign_borders.png",
 		"forsale_sign.png", "forsale_sign.png"},
 	groups = {choppy=2, dig_immediate=2},
-	on_place = determine_sign_type,
+	on_place = economies.switch_on_place {
+		on_ceiling="realestate:forsale_sign_hanging",
+		on_ground="realestate:forsale_sign_yard",
+	},
 	on_construct = forsale_construct,
 	on_receive_fields = forsale_receive_fields,
 	on_punch = forsale_punch
