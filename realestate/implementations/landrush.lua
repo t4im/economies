409dd4f67679b7d7fe31000b2faa372be1a37631
Tim <t4im@users.noreply.github.com>
2015-01-25 15:30:57 +0100

    gracefully retire landrush:sale_blocks

--- realestate/implementations/landrush.lua
+++ realestate/implementations/landrush.lua
@@ -9,3 +9,12 @@ realestate.transfer = function(pos, node, puncher)
 	end
 	return true
 end
+
+-- deprecate the old landrush sale block gracefully
+minetest.register_node("realestate:deprecated_landrush_sale_block", {
+	tiles= { "default_wood.png^[crack:1:16" },
+	groups = { dig_immediate=3, not_in_creative_inventory=1 },
+	drop = "default:wood 5",
+})
+minetest.register_alias("landrush:sale_block", "realestate:deprecated_landrush_sale_block")
+
