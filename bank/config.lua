8011e76a6900576bdb47f0d49d736b976d945e9a
Tim <t4im@users.noreply.github.com>
2015-03-15 16:17:20 +0100

    fix typo in modname checked as dependency

--- bank/config.lua
+++ bank/config.lua
@@ -19,6 +19,6 @@ economies.register_config_defaults({
 })
 
 -- optional dependency support
-economies.with_plastic = minetest.get_modpath("homedecore") ~= nil
-			or minetest.get_modpath("pipeworks") ~= nil -- defines plastic if homedecore is not available
+economies.with_plastic = minetest.get_modpath("homedecor") ~= nil
+			or minetest.get_modpath("pipeworks") ~= nil -- defines plastic if homedecor is not available
 economies.with_compressor = minetest.get_modpath("technic") ~= nil
